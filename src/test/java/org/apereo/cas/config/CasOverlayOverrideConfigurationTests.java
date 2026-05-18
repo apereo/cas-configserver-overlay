ead56bd6c992e4527bd38c257ac8e3364ba45ed7
cas-initializr <info@apereo.org>
2026-05-18 23:04:10 +0000

    Project created by Apereo CAS Initializr

---
+++ src/test/java/org/apereo/cas/config/CasOverlayOverrideConfigurationTests.java
@@ -0,0 +1,15 @@
+package org.apereo.cas.config;
+
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = CasOverlayOverrideConfiguration.class)
+class CasOverlayOverrideConfigurationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
